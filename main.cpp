e87f9c5141fab79efba8292747223f4584e3c518
YuriNeverov <neverov.iury@yandex.ru>
2020-05-16 14:37:48 +0300

    Decoding errors cheching improved.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,25 @@
 
 constexpr uint compression_power = 20;
 
+bool checkDecodingErrors(int decoderExitCode) {
+    switch (decoderExitCode) {
+        case 1:
+            std::cerr << "Cannot open input file for decoding\n";
+            return true;
+        case 2:
+            std::cerr << "Cannot open output file for decoding\n";
+            return true;
+        case 4:
+            std::cerr << "Bad memory allocation while decoding\n";
+            return true;
+        case 8:
+            std::cerr << "Wrong file to decode: hashes don't match\n";
+            return true;
+        default:
+            return false;
+    }
+}
+
 // First arg for input file, second for archive, third one for output
 
 int main(int argc, char *argv[]) {
@@ -21,6 +40,10 @@ int main(int argc, char *argv[]) {
          << time_taken << std::setprecision(5);
     std::cout << " sec " << std::endl;
 
+    int decoderExitCode = LZ77Decoder::decode(argv[2], argv[3]);
 
-    return LZ77Decoder::decode(argv[2], argv[3]);
+    if (checkDecodingErrors(decoderExitCode))
+        return 1;
+
+    return 0;
 }
