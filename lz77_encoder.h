564e5059e64dd068a73d26749756cb5424cf8b6d
bizzareTunez <63678493+bizzareTunez@users.noreply.github.com>
2020-05-16 17:12:18 +0300

    code fixed

--- lz77_encoder.h
+++ lz77_encoder.h
@@ -13,6 +13,10 @@ typedef unsigned short ushort;
 
 class lz77_encoder {
 public:
+    lz77_encoder() {
+        block_buffer = new uchar[BUFFER_SIZE];
+    }
+
     void open_input_file(char *input_file);
 
     void open_output_file(char *output_file);
@@ -37,7 +41,7 @@ private:
     std::ifstream input;
     std::ofstream output;
     array_deque window, buffer;
-    uchar *block_buffer = new uchar[BUFFER_SIZE];
+    uchar *block_buffer;
     ushort buffer_size_processing;
     uint file_size;
     uint *hash;
